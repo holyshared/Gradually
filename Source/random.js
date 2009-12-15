7e2504f6c9778f6160de86a0d348c3d7c4f979cf
holy <holy.shared.design@gmail.com>
2009-12-15 13:19:18 +0900

    add package.yaml and readme.md add sourcecode

--- Source/random.js
+++ Source/random.js
@@ -1,3 +1,25 @@
+/*
+---
+description: Element class, Elements class, and basic dom methods.
+
+license: MIT-style
+
+authors:
+- Jimmy Dean
+- Buck Kingsley
+
+requires:
+- localComponent1
+- [localComponent2, localComponent3]
+- externalPackage1:tag/component1
+- externalPackage1:tag: component4
+- externalPackage2:tag: [component1, component2]
+
+provides: [Element, Elements, $, $$]
+
+...
+*/
+
 var RandomImage = new Class({
 
 	Implements: [Events, Options],
