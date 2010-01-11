c639165c09be721ee22ebdfb01bf8d505ec6743c
Noritaka Horio <holy.shared.design@gmail.com>
2010-01-11 23:54:05 +0900

    yaml header changed.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Source/Gradually.js
+++ Source/Gradually.js
@@ -1,13 +1,36 @@
 /*
 ---
 description: Gradually is an experimental slide show plug-in using the canvas element.
+
 license: MIT-style
+
 authors:
 - Noritaka Horio
+
 requires:
-- core/1.2.4: [Core/*, Native/*, Class/*, Element/*, Utilities/Selecter, Utilities/DomReady, Fx/Fx]
-- more/1.2.4.2: [Assets]
+  core/1.2.4:
+  - Core/Core
+  - Core/Browser,
+  - Native/Array,
+  - Native/Function,
+  - Native/Number,
+  - Native/String,
+  - Native/Hash,
+  - Native/Event,
+  - Class/Class,
+  - Class/Class.Extras,
+  - Element/Element,
+  - Element/Element.Event,
+  - Element/Element.Style,
+  - Element/Element.Dimensions,
+  - Utilities/Selecter,
+  - Utilities/DomReady,
+  - Fx/Fx
+  more/1.2.4.2:
+  - Assets
+
 provides: [Gradually]
+
 ...
 */
 
