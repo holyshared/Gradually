d63101e167b29f1b704c597dba50d8a9c0d98ab0
Noritaka Horio <holy.shared.design@gmail.com>
2010-01-11 21:18:41 +0900

    change scripts headers.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Source/Gradually.js
+++ Source/Gradually.js
@@ -1,13 +1,18 @@
 /*
 ---
 description: Gradually is an experimental slide show plug-in using the canvas element.
+
 license: MIT-style
+
 authors:
 - Noritaka Horio
+
 requires:
-- core/1.2.4: [Core/*, Native/*, Class/*, Element/*, Utilities/Selecter, Utilities/DomReady, Fx/Fx]
-- more/1.2.4.2: [Assets]
+- core/1.2.4
+- more/1.2.4.2
+
 provides: [Gradually]
+
 ...
 */
 
