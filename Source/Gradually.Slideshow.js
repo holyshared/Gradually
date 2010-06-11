4933021cee615701a2539fbc5ab970222be2c58b
holy <holy.shared.design@gmail.com>
2010-06-11 10:58:56 +0900

    add Slideshow
    Signed-off-by: holy <holy.shared.design@gmail.com>

---
+++ Source/Gradually.Slideshow.js
@@ -0,0 +1,44 @@
+/*
+---
+description: Gradually is an experimental slide show plug-in using the canvas element.
+
+license: MIT-style
+
+authors:
+- Noritaka Horio
+
+requires:
+  core/1.2.4:
+  - Core/Core
+  - Core/Browser
+  - Native/Array
+  - Native/Function
+  - Native/Number
+  - Native/String
+  - Native/Hash
+  - Native/Event
+  - Class/Class
+  - Class/Class.Extras
+  - Element/Element
+  - Element/Element.Event
+  - Element/Element.Style
+  - Element/Element.Dimensions
+  - Utilities/Selecter
+  - Utilities/DomReady
+  - Fx/Fx
+  more/1.2.4.2:
+  - Assets
+
+provides: [Gradually]
+
+...
+*/
+
+var Gradually.Slideshow = new Class({
+
+	Implements: [Events, Options],
+
+	initialize: function (container,sources,options) {
+	}
+
+});
