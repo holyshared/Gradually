3d6a951b50235c17a6a6107e53b7b221d349c42e
Noritaka Horio <holy.shared.design@gmail.com>
2010-10-14 02:54:12 +0900

    demo page changed.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Demos/js/demo.js
+++ Demos/js/demo.js
@@ -1,25 +1,25 @@
-window.addEvent("domready", function(){
+window.addEvent('domready', function(){
 
 	var container = null, sources = null;
 	var options = {
-		'panelHeight': 55,
-		'panelWidth': 65,
-		'interval': 3000,
-		'duration': 1000,
-		'zIndex': 9000,
-		'onStart': function() {
+		panelHeight: 55,
+		panelWidth: 65,
+		interval: 3000,
+		duration: 1000,
+		zIndex: 9000,
+		onStart: function() {
 			$("container").getElement("p.information").set("html", "now loading....");
 		},
-		'onPreload': function(images) {
+		onPreload: function(images) {
 			$("container").getElement("p.information").set("html", images.length.toString() + "loaded");
 		},
-		'onChange': function(image) {
+		onChange: function(image) {
 			$("container").getElement("p.information").set("html", image.title + " : " + image.alt);
 		}
 	};
 	
-	var container	= $("gradually-container");
-	var sources		= $("gradually-container").getElements("img");
+	var container	= $('gradually-container');
+	var sources		= container.getElements('img');
 	new Gradually(container, sources, options);
 	
 });
